610e9d48dd1201d9c353a696b2c39a7000221128
Yogya2502 <114391409+Yogya2502@users.noreply.github.com>
2022-11-01 02:45:44 +0530

    Simple interest and Amount

---
+++ Simple_Interest.c
@@ -0,0 +1,25 @@
+/*calculation of simple interest*/
+#include <stdio.h>
+
+int main()
+{
+    int p,n;
+    float r,si,amount;
+
+    p = 100;
+    n = 4;
+    r = 10;
+
+    /* Formula of Simple Interest*/
+
+    si = p*n*r/100;
+
+    printf("%f\n",si);
+
+    amount = p + si;
+  
+    printf("%f\n",amount);
+  
+    return 0;
+
+}
